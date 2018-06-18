8ed82fda0320214899d17094aaff02056585047b
nashsclay <nashsclay@gmail.com>
2018-06-17 22:49:37 -0700

    Update shmn_testmn_setup_script.sh

--- shmn_testmn_setup_script.sh
+++ shmn_testmn_setup_script.sh
@@ -200,7 +200,7 @@ apt-add-repository -y ppa:bitcoin/bitcoin #>/dev/null 2>&1
 echo -e "Installing required packages, it may take some time to finish.${NC}"
 apt-get update #>/dev/null 2>&1
 apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
-build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-all-dev \
+build-essential libtool unzip autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-all-dev \
 libdb4.8 libdb4.8++ libboost-system1.58.0 libboost-filesystem1.58.0 \
 libboost-program-options1.58.0 libboost-thread1.58.0 libssl1.0.0 libminiupnpc10 libevent-2.0-5 \
 libevent-pthreads-2.0-5 libevent-core-2.0-5 libminiupnpc-dev libzmq3-dev git nano tmux curl wget pwgen libzmq3-dev libboost-all-dev libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev #>/dev/null 2>&1
