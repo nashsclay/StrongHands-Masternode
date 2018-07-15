8fcd044ed6ecabf7309e566b92386247fedeb9b9
nashsclay <nashsclay@gmail.com>
2018-07-15 11:15:13 -0700

    Update shmn_mn_setup.sh

--- shmn_mn_setup.sh
+++ shmn_mn_setup.sh
@@ -23,15 +23,15 @@ NC='\033[0m'
 
 function download_node() {
   echo -e "Prepare to download ${GREEN}$COIN_NAME${NC}."
-  cd $TMP_FOLDER #>/dev/null 2>&1
+  cd $TMP_FOLDER >/dev/null 2>&1
   wget -q $COIN_TGZ
   compile_error
-  unzip $COIN_ZIP #>/dev/null 2>&1
+  unzip $COIN_ZIP >/dev/null 2>&1
   #cd bin #verify
   chmod +x $COIN_DAEMON $COIN_CLI
   cp $COIN_DAEMON $COIN_CLI $COIN_PATH
-  cd ~ #>/dev/null
-  rm -rf $TMP_FOLDER #>/dev/null 2>&1
+  cd ~ >/dev/null
+  rm -rf $TMP_FOLDER >/dev/null 2>&1
   read -p "Hello"
   clear
 }
@@ -62,7 +62,7 @@ EOF
   systemctl daemon-reload
   sleep 3
   systemctl start $COIN_NAME.service
-  systemctl enable $COIN_NAME.service #>/dev/null 2>&1
+  systemctl enable $COIN_NAME.service >/dev/null 2>&1
 
   if [[ -z "$(ps axo cmd:100 | egrep $COIN_DAEMON)" ]]; then
     echo -e "${RED}$COIN_NAME is not running${NC}, please investigate. You should start by running the following commands as root:"
@@ -75,7 +75,7 @@ EOF
 
 
 function create_config() {
-  mkdir $CONFIGFOLDER #>/dev/null 2>&1
+  mkdir $CONFIGFOLDER >/dev/null 2>&1
   RPCUSER=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w10 | head -n1)
   RPCPASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w22 | head -n1)
   cat << EOF > $CONFIGFOLDER/$CONFIG_FILE
@@ -132,11 +132,11 @@ EOF
 
 function enable_firewall() {
   echo -e "Installing and setting up firewall to allow ingress on port ${GREEN}$COIN_PORT${NC}"
-  ufw allow $COIN_PORT/tcp comment "$COIN_NAME MN port" #>/dev/null
-  ufw allow ssh comment "SSH" #>/dev/null 2>&1
-  ufw limit ssh/tcp #>/dev/null 2>&1
-  ufw default allow outgoing #>/dev/null 2>&1
-  echo "y" | ufw enable #>/dev/null 2>&1
+  ufw allow $COIN_PORT/tcp comment "$COIN_NAME MN port" >/dev/null
+  ufw allow ssh comment "SSH" >/dev/null 2>&1
+  ufw limit ssh/tcp >/dev/null 2>&1
+  ufw default allow outgoing >/dev/null 2>&1
+  echo "y" | ufw enable >/dev/null 2>&1
 }
 
 
@@ -192,20 +192,20 @@ fi
 
 function prepare_system() {
 echo -e "Prepare the system to install ${GREEN}$COIN_NAME${NC} master node."
-apt-get update #>/dev/null 2>&1
+apt-get update >/dev/null 2>&1
 DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
-DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade #>/dev/null 2>&1 
-apt install -y software-properties-common #>/dev/null 2>&1 
+DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1 
+apt install -y software-properties-common >/dev/null 2>&1 
 echo -e "${GREEN}Adding bitcoin PPA repository"
-apt-add-repository -y ppa:bitcoin/bitcoin #>/dev/null 2>&1
+apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
 echo -e "Installing required packages, it may take some time to finish.${NC}"
-apt-get update #>/dev/null 2>&1
+apt-get update >/dev/null 2>&1
 apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
 build-essential libtool unzip autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-all-dev \
 libdb4.8 libdb4.8++ libboost-system1.58.0 libboost-filesystem1.58.0 \
 libboost-program-options1.58.0 libboost-thread1.58.0 libssl1.0.0 libminiupnpc10 libevent-2.0-5 \
-libevent-pthreads-2.0-5 libevent-core-2.0-5 libminiupnpc-dev libzmq3-dev git nano tmux curl wget pwgen libzmq3-dev libboost-all-dev libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev #>/dev/null 2>&1
-apt-get install -y libgmp3-dev #>/dev/null 2>&1
+libevent-pthreads-2.0-5 libevent-core-2.0-5 libminiupnpc-dev libzmq3-dev git nano tmux curl wget pwgen libzmq3-dev libboost-all-dev libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev >/dev/null 2>&1
+apt-get install -y libgmp3-dev >/dev/null 2>&1
 
 if [ "$?" -gt "0" ];
   then
